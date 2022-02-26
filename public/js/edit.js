a9cd1e4cc8863e06fc8d2d42b862eef54db0f825
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-02-26 09:38:56 -0800

    Implemented Edit Post functionality.

--- public/js/edit.js
+++ public/js/edit.js
@@ -4,9 +4,15 @@ const editFormHandler = async (event) => {
     // const title = document.querySelector('#edit-title').value.trim();
     // const content = document.querySelectorAll('textarea').value.trim();
 
-    const title = document.querySelector('#post-title').value.trim();
-    const content = document.querySelector('input[name="post-content"]').value.trim();
-  
+    const title = document.querySelector('#edit-title').value.trim();
+    // const content = document.querySelector('textarea[name="editContent"]').value.trim();
+    const content = document.getElementsByName('editContent')[0].value;
+
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+
+
     // NEED TO MAKE SURE USER IS LOGGED IN.
     // if (title && content) {
       const response = await fetch(`/api/posts/${id}`, {
